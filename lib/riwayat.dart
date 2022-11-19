1048616a0c65b3ae514554ba5a715c4419bd2bd6
Yudas <yudasmalabi@gmail.com>
2022-11-19 14:41:28 +0700

    Done Konversi Suhu List With Screenshots

---
+++ lib/riwayat.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RiwayatKonversi extends StatelessWidget {
+  const RiwayatKonversi({
+    Key? key,
+    required this.listViewItem,
+  }) : super(key: key);
+
+  final List<String> listViewItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: listViewItem.map((String value) {
+        return Container(
+            margin: const EdgeInsets.all(10),
+            child: Text(
+              value,
+              style: const TextStyle(fontSize: 15),
+            ));
+      }).toList(),
+    );
+  }
+}
